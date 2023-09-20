8c1f9ad9312b07e01ec9da18935ece93b6032b18
Amarachi Dimkpa <amaradimkpa1@gmail.com>
2023-09-20 08:36:35 -0400

    Modified app/javascript/application.js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,12 +1,39 @@
-// // Entry point for the build script in your package.json
+// Entry point for the build script in your package.json
 // import "@hotwired/turbo-rails"
 // import "./controllers"
 
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import store from "./app/store";
+import Greeting from "./components/Greeting";
 
 function App() {
-  return (<h1>Hello World!</h1>);
+  return (
+    <div>
+      <h2>Welcome to React + Rails</h2>
+      <small>Navigate to '/greeting' endpoint to see random message...</small>
+    </div>
+  );
 }
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/greeting",
+    element: <Greeting />,
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </React.StrictMode>
+);
